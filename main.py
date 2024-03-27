8ab3421b9a885180a6113519b9476925ba0f43bd
rsi2024 <164152459+rsi2024@users.noreply.github.com>
2024-03-27 13:14:13 +0300

    (all cycle code) main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 import random
-
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -20,9 +19,19 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 250))
 
+running = True
+while running:
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-ruming = True
-while ruming:
-    pass
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
