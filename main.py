ae5c1661a4f7f10fca3caa11f8fa5a1b941da892
rsi2024 <164152459+rsi2024@users.noreply.github.com>
2024-03-27 14:10:13 +0300

    (new) main.py

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 250))
+score = 0
+
+font = pygame.font.Font(None, 36)
 
 running = True
 while running:
@@ -28,10 +31,14 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                score += 1
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
     screen.blit(target_img, (target_x, target_y))
+    score_text = font.render(f'Очки: {score}', True, (255, 255, 255))
+
+    screen.blit(score_text, (10, 10))
     pygame.display.update()
 
 pygame.quit()
